c694a0fa60ad0fa8f2e23fcf972841aba227bf18
marcinkaczmarek2 <titnet112pl@gmail.com>
2025-06-10 10:52:15 +0200

    small fix in wait time value

--- src/frontend/panels/UserPanel.cpp
+++ src/frontend/panels/UserPanel.cpp
@@ -35,7 +35,7 @@ void UserPanel::setLoggedInUser(User* user) {
 }
 
 void UserPanel::clearConsole() {
-    std::this_thread::sleep_for(std::chrono::seconds(0.5));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
 #ifdef _WIN32
     system("cls");
 #elif defined(__unix__) || defined(__APPLE__) || defined(__MACH__)
